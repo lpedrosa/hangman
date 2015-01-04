fb24cecfde2c7676e3c5d8a18f45023e25123903
lpedrosa <lpedrosa88@gmail.com>
2015-01-04 00:06:45 +0000

    skeleton of game manager

---
+++ src/main/java/io/github/lpedrosa/GameManager.java
@@ -0,0 +1,9 @@
+package io.github.lpedrosa;
+
+public final class GameManager {
+
+	// guess :: Letter GameId -> Game
+	// saveState :: Game -> Game, SaveStateException
+	// loadState :: GameId -> Game, LoadStateException
+	// listGames :: ()
+}
--- src/main/java/io/github/lpedrosa/domain/Game.java
+++ src/main/java/io/github/lpedrosa/domain/Game.java
@@ -1,19 +1,40 @@
 package io.github.lpedrosa.domain;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 public final class Game {
+	
+	private final long gameId;
 	private final String currentWord;
 	private final Map<String, List<Integer>> uncoveredByPostion;
 	private final int livesLeft;
 
-	public Game(String currentWord,
+	public Game(long gameId,
+				String currentWord,
 				Map<String, List<Integer>> uncoveredByPostion, 
 				int livesLeft) {
+		this.gameId = gameId;
 		this.currentWord = currentWord;
 		this.uncoveredByPostion = uncoveredByPostion;
 		this.livesLeft = livesLeft;
+	}
+	
+	public long getGameId() {
+		return gameId;
+	}
+
+	public String getCurrentWord() {
+		return currentWord;
+	}
+
+	public Map<String, List<Integer>> getUncoveredByPostion() {
+		return Collections.unmodifiableMap(uncoveredByPostion);
+	}
+
+	public int getLivesLeft() {
+		return livesLeft;
 	}
 	
 }
